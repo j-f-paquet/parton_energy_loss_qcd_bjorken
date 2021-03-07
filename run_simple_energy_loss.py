447369ac0aa7268e6f97e666d4f56f03d7282c6a
Matthew Heffernan <matthew.heffernan@mail.mcgill.ca>
2021-03-06 21:57:19 -0500

    Adding timeout

--- run_simple_energy_loss.py
+++ run_simple_energy_loss.py
@@ -14,6 +14,18 @@ from solver_rk import parton_evolution_solver_rk
 
 import time
 
+
+import signal
+
+class TimeoutException(Exception):   # Custom exception class
+    pass
+
+def timeout_handler(signum, frame):   # Custom signal handler
+    raise TimeoutException
+
+# Change the behavior of SIGALRM
+signal.signal(signal.SIGALRM, timeout_handler)
+
 hbarc=0.1973
 
 
@@ -36,83 +48,91 @@ def run_simulation(design_matrix, p_min=1, p_max=20, num_p_bins=20):
         print(f'Working on {ii+1}/{design_matrix.shape[0]} design')
         start_time = time.time()
         
-        params=params.flatten()
+        signal.alarm(600) # time out after 10 minutes
+        try:
+            params=params.flatten()
 
-        param_dict={
-        'T0_in_GeV':0.3,
-        'tau0':0.2,
-        'T_final_in_GeV':0.15,
-        'alpha_s':params[0],
-        'N_f':0,
-        'mD_factor':1.0,
-        'exponent_inel':params[1],
-        'exponent_el':params[2],
-        'RAA_pT_binnings':np.linspace(p_min, p_max, num_p_bins),
-        'scale_inel':params[3],
-        'scale_el':params[4]
-        }
+            param_dict={
+            'T0_in_GeV':0.3,
+            'tau0':0.2,
+            'T_final_in_GeV':0.15,
+            'alpha_s':params[0],
+            'N_f':0,
+            'mD_factor':1.0,
+            'exponent_inel':params[1],
+            'exponent_el':params[2],
+            'RAA_pT_binnings':np.linspace(p_min, p_max, num_p_bins),
+            'scale_inel':params[3],
+            'scale_el':params[4]
+            }
 
-        T0_in_GeV=param_dict['T0_in_GeV']
-        tau0=param_dict['tau0']
-        T_final_in_GeV=param_dict['T_final_in_GeV']
-        alpha_s=param_dict['alpha_s']
-        N_f=param_dict['N_f']
-        mD_factor=param_dict['mD_factor']
-        exponent_inel=param_dict['exponent_inel']
-        exponent_el=param_dict['exponent_el']
-        RAA_pT_binnings=param_dict['RAA_pT_binnings']
-        # The new parameters
-        scale_inel=param_dict['scale_inel']
-        scale_el=param_dict['scale_el']
+            T0_in_GeV=param_dict['T0_in_GeV']
+            tau0=param_dict['tau0']
+            T_final_in_GeV=param_dict['T_final_in_GeV']
+            alpha_s=param_dict['alpha_s']
+            N_f=param_dict['N_f']
+            mD_factor=param_dict['mD_factor']
+            exponent_inel=param_dict['exponent_inel']
+            exponent_el=param_dict['exponent_el']
+            RAA_pT_binnings=param_dict['RAA_pT_binnings']
+            # The new parameters
+            scale_inel=param_dict['scale_inel']
+            scale_el=param_dict['scale_el']
 
-        #################################################
-        ############## Temperature profile ##############
-        ##################################################
+            #################################################
+            ############## Temperature profile ##############
+            ##################################################
 
-        #T_profile=brick_profile(T0_in_GeV=T0_in_GeV)
-        T_profile=Bjorken_hydro_profile(T0_in_GeV=T0_in_GeV, tau0=tau0)
+            #T_profile=brick_profile(T0_in_GeV=T0_in_GeV)
+            T_profile=Bjorken_hydro_profile(T0_in_GeV=T0_in_GeV, tau0=tau0)
 
-        ######################################################
-        ############## Parton energy loss rates ##############
-        ######################################################
+            ######################################################
+            ############## Parton energy loss rates ##############
+            ######################################################
 
-        
-        K_factor_fct_inel=lambda T, scale_inel=scale_inel, exponent_inel=exponent_inel : (1.+np.power(T/scale_inel,exponent_inel))
-        K_factor_fct_elastic=lambda T, scale_el=scale_el, exponent_el=exponent_el : (1.+np.power(T/scale_el,exponent_el))
-        energy_loss_rate=energy_loss_rates(alpha_s = alpha_s, N_f=N_f, mD_factor=mD_factor, K_factor_fct_inel=K_factor_fct_inel, K_factor_fct_elastic=K_factor_fct_elastic)
-        #######################################################
-        ############## Parton energy loss solver ##############
-        #######################################################
 
-        # Initial conditions 
-        def P_g_tau0(p):
+            K_factor_fct_inel=lambda T, scale_inel=scale_inel, exponent_inel=exponent_inel : (1.+np.power(T/scale_inel,exponent_inel))
+            K_factor_fct_elastic=lambda T, scale_el=scale_el, exponent_el=exponent_el : (1.+np.power(T/scale_el,exponent_el))
+            energy_loss_rate=energy_loss_rates(alpha_s = alpha_s, N_f=N_f, mD_factor=mD_factor, K_factor_fct_inel=K_factor_fct_inel, K_factor_fct_elastic=K_factor_fct_elastic)
+            #######################################################
+            ############## Parton energy loss solver ##############
+            #######################################################
 
-            p0=1.75
+            # Initial conditions 
+            def P_g_tau0(p):
+
+                p0=1.75
+
+                return np.power(p0*p0+p*p,-5.)
+
+
+            # Initialize and use the solver
+            num_p_solver=num_p_bins
+            pmin_solver=p_min
+            pmax_solver=p_max
+            #parton_evolution_solver=parton_evolution_solver_euler(initial_condition_fct=P_g_tau0, tau0=tau0, T_profile=T_profile, energy_loss_rate=energy_loss_rate, num_p=num_p_solver, pmin=pmin_solver, pmax=pmax_solver)
+            #P_final_fct=parton_evolution_solver.evolve_to_min_temperature(dtau=dtau_adaptive, T_min_in_GeV=T_final_in_GeV, use_adaptive_timestep=True)
+            parton_evolution_solver=parton_evolution_solver_rk(initial_condition_fct=P_g_tau0, tau0=tau0, T_profile=T_profile, energy_loss_rate=energy_loss_rate, num_p=num_p_solver, pmin=pmin_solver, pmax=pmax_solver)
+            P_final_fct=parton_evolution_solver.evolve_to_min_temperature(T_min_in_GeV=T_final_in_GeV)
+
+            # Compute some "RAA"-equivalent
+            P_initial=P_g_tau0(RAA_pT_binnings)
+            P_final=P_final_fct(RAA_pT_binnings)
+            result=P_final/P_initial
+            end_time = time.time()
+            delta_t = end_time - start_time
+
+            if  delta_t > 60 :
+                print(f'For model parameters {params} takes {delta_t} S')
+
+            observations.append(result)
             
-            return np.power(p0*p0+p*p,-5.)
-
-
-        # Initialize and use the solver
-        num_p_solver=num_p_bins
-        pmin_solver=p_min
-        pmax_solver=p_max
-        #parton_evolution_solver=parton_evolution_solver_euler(initial_condition_fct=P_g_tau0, tau0=tau0, T_profile=T_profile, energy_loss_rate=energy_loss_rate, num_p=num_p_solver, pmin=pmin_solver, pmax=pmax_solver)
-        #P_final_fct=parton_evolution_solver.evolve_to_min_temperature(dtau=dtau_adaptive, T_min_in_GeV=T_final_in_GeV, use_adaptive_timestep=True)
-        parton_evolution_solver=parton_evolution_solver_rk(initial_condition_fct=P_g_tau0, tau0=tau0, T_profile=T_profile, energy_loss_rate=energy_loss_rate, num_p=num_p_solver, pmin=pmin_solver, pmax=pmax_solver)
-        P_final_fct=parton_evolution_solver.evolve_to_min_temperature(T_min_in_GeV=T_final_in_GeV)
-
-        # Compute some "RAA"-equivalent
-        P_initial=P_g_tau0(RAA_pT_binnings)
-        P_final=P_final_fct(RAA_pT_binnings)
-        result=P_final/P_initial
-        end_time = time.time()
-        delta_t = end_time - start_time
-        
-        if  delta_t > 60 :
-            print(f'For model parameters {params} takes {delta_t} S')
-        
-        observations.append(result)
-        
+        except TimeoutException: # what to do if it takes longer than 10 minutes
+            print(f'For model parameters {params} takes more than 10 mins')
+            observations.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        else:
+            # Reset the alarm
+            signal.alarm(0)
     observations=np.array(observations)
     return observations
 
