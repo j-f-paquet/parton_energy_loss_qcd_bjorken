9fcf9b64d1103cdff879f1f5d1f07c0ecd0aab13
IreneJi0407 <jiyi020@hotmail.com>
2021-10-13 14:24:36 -0400

    update contour_MultiObs

--- contour_MultiObs.py
+++ contour_MultiObs.py
@@ -4,7 +4,8 @@ import numpy as np
 
 from ...core.acquisition import Acquisition
 
-def EI_1Obs(m, s, a, alpha):
+def EI_1Obs(m, s2, a, alpha):
+    s = np.sqrt(s2)
     f = lambda y: (y-m)**2 * norm.pdf((y-m)/s) # integrand for the third term
     f_acqu_x_temp = ((alpha**2 * s2) - (m-a)**2) * (norm.cdf((a-m)/s+alpha) - norm.cdf((a-m)/s-alpha)) \
             + 2*(m-a)*s2 * (norm.pdf((a-m)/s+alpha) - norm.pdf((a-m)/s-alpha)) \
@@ -18,15 +19,15 @@ class contourWeightedEI(Acquisition):
     This acquisition is extended from Sequential Experimental Design for
     Contour Estimation (Ranjan et al. 2008). It works for multiple output problem. It has not taken the noise of experimental data into account yet.
     """
-    def __init__(self, models, a_list, alpha_list, w_list) -> None:
+    def __init__(self, modelwrapper, a_list, alpha_list, w_list) -> None:
 
         """
-        :param multioutputmodelwrapper: One model for multiple inputs and multiple outputs, combining multiple independent models
+        :param modelwrapper: One model for multiple inputs and multiple outputs, combining multiple independent models
         :param a_list : Target observable evalues, one for each observable
         :param alpha_list: Control the sampling of the regions with highest variance, one for each observable
         :param w_list: Weights of EI put on each observable
         """
-        self.models = models
+        self.modelwrapper = modelwrapper
         self.a_list = a_list
         self.alpha_list = alpha_list
         self.w_list = w_list
@@ -34,25 +35,27 @@ class contourWeightedEI(Acquisition):
     def evaluate(self, x: np.ndarray) -> np.ndarray:
         
         '''
-        Idea: have a list of models as inputs, one model corresponding to one observable, calculate EI(x) for each model, then combine them for final EI to be optimized (in this acquisition function we use the weighted sum of all EI's)
+        Idea: have a model wrapper for a set of independent GPs, one model corresponding to one observable, calculate EI(x) for each model, then combine them for final EI to be optimized (in this acquisition function we use the weighted sum of all EI's)
         '''
         
         # save predictive means and variances of all observables in 2 dictionaries
         m_dict = dict(); s2_dict = dict()
-        for i in range(len(a_list)):
-            m_vec = models.predict(x)[0][i]
-            s2_vec = models.predict(x)[1][i]
+        for i in range(len(self.a_list)):
+            new_column = [[i]]*len(x) # predict for ith emulator
+            pred_temp = self.modelwrapper.predict(np.append(x,new_column,axis=1))
+            m_vec = pred_temp[0] # vector of predictive means
+            s2_vec = pred_temp[1] # vector of predictive variances
             m_dict[i] = m_vec; s2_dict[i] = s2_vec
 
         f_acqu_x = []
-        for index in range(len(m_dict[0])): # same number of x for each observable
-            EI_eachObs = [] # for each x, calculate EI of each observable
-            for i in range(len(self.model_list)):
+        for index in range(len(x)): # for each x, calculate EI of each observable
+            EI_eachObs = []
+            for i in range(len(self.a_list)):
                 a = self.a_list[i]
                 alpha = self.alpha_list[i]
-                m = m_dict[i][index]; s2 = s2_dict[i][index]; s = np.sqrt(s2)
-                EI_eachObs.append(EI_1Obs(m, s, a, alpha))
-            f_acqu_x.append(sum(map(lambda wi, EIi: wi * EIi, w_list, EI_eachObs))) # Approach 2, return weighted sum
+                m = m_dict[i][index]; s2 = s2_dict[i][index]
+                EI_eachObs.append(EI_1Obs(m, s2, a, alpha))
+            f_acqu_x.append(sum(map(lambda wi, EIi: wi * EIi, self.w_list, EI_eachObs))) # Approach 2, return weighted sum
 
         f_acqu_x = np.array(f_acqu_x); f_acqu_x = f_acqu_x.reshape(-1,1)
 
